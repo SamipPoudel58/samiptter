f5c80c3fc2dec372ec2dfb64dc973a660875f973
Samip Poudel <sawmeep58@gmail.com>
2021-04-17 15:57:41 +0545

    created api endpoint to get profile data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const Tweet = require("../models/tweetModel");
 const asyncHandler = require("express-async-handler");
 const generateToken = require("../utils/generateToken.js");
 
@@ -58,7 +59,24 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Get User Profile
+// @route GET /api/users/:id
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const user = await User.findById(id);
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+  const tweets = await Tweet.find({ user: id });
+  res.json(user, tweets);
+});
+
 module.exports = {
   loginUser,
   registerUser,
+  getUserProfile,
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { loginUser, registerUser } = require("../controllers/userController");
+const {
+  loginUser,
+  registerUser,
+  getUserProfile,
+} = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/login", loginUser);
 router.route("/").post(registerUser);
+router.route("/:id").get(protect, getUserProfile);
 
 module.exports = router;
