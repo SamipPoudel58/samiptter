b69f596bfd0d52b717d1b97ff1019d8e4ab8f566
Samip Poudel <sawmeep58@gmail.com>
2021-10-03 17:29:21 +0545

    feat: add showcase sections for important links

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -117,6 +117,40 @@ const Layout = ({ children }) => {
       <section className="rightNav__wrapper">
         <div className="rightNav">
           <FollowRecommendation />
+
+          <a
+            href="https://github.com/SamipPoudel58/samiptter"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <article className="rightNav__showcase">
+              <p className="rightNav__showcaseIntro">
+                This project is open-sourced on github.
+              </p>
+              <p className="rightNav__showcaseLink">Check it out &rarr;</p>
+              <img
+                className="rightNav__showcaseImage"
+                src="/images/github.png"
+                alt="github logo"
+              />
+            </article>
+          </a>
+
+          <a href="https://samippoudel.com.np" target="_blank" rel="noreferrer">
+            <article className="rightNav__showcase rightNav__showcase-creator">
+              <p className="rightNav__showcaseIntro rightNav__showcaseIntro-creator">
+                Hi! I am Samip. Check out my other projects.
+              </p>
+              <p className="rightNav__showcaseLink rightNav__showcaseLink-creator">
+                See More &rarr;
+              </p>
+              <img
+                className="rightNav__showcaseImage rightNav__showcaseImage-creator"
+                src="/images/profile.jpg"
+                alt="profile logo"
+              />
+            </article>
+          </a>
         </div>
       </section>
     </>
