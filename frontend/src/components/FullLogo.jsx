5e062e816a3144a4ba11923b04ad3d195c6e2849
Samip Poudel <sawmeep58@gmail.com>
2021-10-23 22:59:53 +0545

    fix: add link in logo

--- frontend/src/components/FullLogo.jsx
+++ frontend/src/components/FullLogo.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { ReactComponent as Logo } from "../assets/logo.svg";
+import { Link } from "react-router-dom";
 const FullLogo = () => {
   return (
-    <div className="sideNav__logoHolder mb-2">
-      <Logo className="sideNav__logo" />
-      <h1 className="logo-text">Samiptter</h1>
-    </div>
+    <Link to="/">
+      <div className="sideNav__logoHolder mb-2">
+        <Logo className="sideNav__logo" />
+        <h1 className="logo-text">Samiptter</h1>
+      </div>
+    </Link>
   );
 };
 
