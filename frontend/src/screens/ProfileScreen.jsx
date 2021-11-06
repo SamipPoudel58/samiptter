f912d875031294aab53c907b81a4d031e2efbaff
Samip Poudel <sawmeep58@gmail.com>
2021-11-06 19:31:53 +0545

    style: restyle image previews

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -109,10 +109,24 @@ const ProfileScreen = ({ history, match }) => {
                     onClick={() => setPreview(false)}
                     className="tweet__backDrop tweet__backDrop-dark"
                   ></div>
-                  <i
+                  <span
                     onClick={() => setPreview(false)}
-                    className="fas fa-times profileMain__previewClose"
-                  ></i>
+                    className="profileMain__previewClose"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
+
                   <img
                     className={`profileMain__popupImage ${
                       preview === "cover"
