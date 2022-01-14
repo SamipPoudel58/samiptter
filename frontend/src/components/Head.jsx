b64e9d138146b99568552536f8abd3b7292d8e16
Samip Poudel <sawmeep58@gmail.com>
2022-01-14 13:29:49 +0545

    fix: add open graph data for all pages

--- frontend/src/components/Head.jsx
+++ frontend/src/components/Head.jsx
@@ -10,8 +10,21 @@ const Head = ({ title, ogtitle, description, image, url, typeOfContent }) => {
       <meta name="description" content={description} />
       <meta
         name="keyword"
-        content="The ultimate social media site of your dreams | Let your social life take off!"
+        content="Samiptter social media twitter clone react node mongodb Samip Poudel open source"
       ></meta>
+
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+
+      <meta property="og:image" content={image} />
+      <meta property="og:image:secure_url" content={image} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:image" content={image} />
+      <meta property="twitter:site" content="@samip4sure" />
+      <meta property="twitter:creator" content="@samip4sure" />
     </Helmet>
   );
 };
@@ -21,7 +34,8 @@ Head.defaultProps = {
   ogtitle: "Samiptter | The Ultimate Social Media Site",
   description:
     "The ultimate social media site of your dreams | Let your social life take off!",
-  image: "https://samiptter.herokuapp.com/images/open_graph.png",
+  image:
+    "https://res.cloudinary.com/samip58/image/upload/v1642145313/samip/samiptter_1_yndgxz.png",
   url: "https://samiptter.herokuapp.com",
   typeOfContent: "website",
 };
